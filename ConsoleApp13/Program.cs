d16bb45c76fb6cc617d1750c08512d8be9de40ea
melika <melikatorabi031@gmail.com>
2023-04-15 22:04:23 +0430

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            int X = Convert.ToInt32(Console.ReadLine());
+            Calc(X);
+        }//end Main
+        private static void Calc(int X)
+        {
+            double Result = Math.Pow(X, 2) + (2 * X) - 4;
+            Console.WriteLine(Result);
+        }//end function
+    }//end class
+
+}//end namespace
